e6719b44eca5943d5c6c80aff096524716ca3f7f
Rodrigo Ramirez <rodrigopinheiroramirez@gmail.com>
2025-01-23 14:35:21 -0300

    Domain model

--- src/main/java/com/rramirez/dslist/entities/Game.java
+++ src/main/java/com/rramirez/dslist/entities/Game.java
@@ -1,13 +1,10 @@
 package com.rramirez.dslist.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode(of = "id")
+
+import java.util.Objects;
+
 @Entity
 @Table(name = "game")
 public class Game {
@@ -33,4 +30,105 @@ public class Game {
 
     @Column(columnDefinition = "TEXT")
     private String longDescription;
+
+    public Game(){
+
+    }
+
+    public Game(Long id, String title, Integer gameYear, String genre, String platforms,
+                Double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.gameYear = gameYear;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getGameYear() {
+        return gameYear;
+    }
+
+    public void setGameYear(Integer gameYear) {
+        this.gameYear = gameYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return Objects.equals(getId(), game.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
